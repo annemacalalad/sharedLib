775aa273fbe2686e76a3c6ef1224f0ed9559200a
annemacalalad <91460116+annemacalalad@users.noreply.github.com>
2021-10-12 16:32:10 +0800

    Create ec2Deploy.groovy

---
+++ vars/ec2Deploy.groovy
@@ -0,0 +1,13 @@
+def call(Map stageParams = [awsRegion: "us-east-1"]){
+  withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {
+        awsIdentity()
+        cfnCreateChangeSet(
+          stack:'EC2Jenkins', 
+          changeSet:'my-change-set', 
+          url:'https://testbucket-anne.s3.amazonaws.com/deployEC2.yml'
+        )
+        cfnExecuteChangeSet(
+          stack:'EC2Jenkins', 
+          changeSet:'my-change-set'
+        )
+  }
